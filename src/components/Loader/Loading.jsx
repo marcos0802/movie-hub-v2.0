2f07445be9524d753ff1bfe2616666122839630e
root <root@localhost.localdomain>
2021-05-15 16:04:20 +0100

    Setup the loader component

---
+++ src/components/Loader/Loading.jsx
@@ -0,0 +1,124 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  .lds-grid {
+    display: inline-block;
+    position: absolute;
+    z-index: 1;
+    width: calc(45%);
+    top: 45%;
+    transform: translate(90%, 70%);
+  }
+  .lds-grid div {
+    position: absolute;
+    width: 20px;
+    height: 20px;
+    border-radius: 50%;
+    background: #10314d;
+    animation: lds-grid 1.2s linear infinite;
+  }
+  .lds-grid div:nth-child(1) {
+    top: 8px;
+    left: 8px;
+    animation-delay: 0s;
+  }
+  .lds-grid div:nth-child(2) {
+    top: 8px;
+    left: 32px;
+    animation-delay: -0.4s;
+  }
+  .lds-grid div:nth-child(3) {
+    top: 8px;
+    left: 56px;
+    animation-delay: -0.8s;
+  }
+  .lds-grid div:nth-child(4) {
+    top: 32px;
+    left: 8px;
+    animation-delay: -0.4s;
+  }
+  .lds-grid div:nth-child(5) {
+    top: 32px;
+    left: 32px;
+    animation-delay: -0.8s;
+  }
+  .lds-grid div:nth-child(6) {
+    top: 32px;
+    left: 56px;
+    animation-delay: -1.2s;
+  }
+  .lds-grid div:nth-child(7) {
+    top: 56px;
+    left: 8px;
+    animation-delay: -0.8s;
+  }
+  .lds-grid div:nth-child(8) {
+    top: 56px;
+    left: 32px;
+    animation-delay: -1.2s;
+  }
+  .lds-grid div:nth-child(9) {
+    top: 56px;
+    left: 56px;
+    animation-delay: -1.6s;
+  }
+  @keyframes lds-grid {
+    0%,
+    100% {
+      opacity: 1;
+    }
+    50% {
+      opacity: 0.5;
+    }
+  }
+`;
+
+const Grids = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  flex-wrap: wrap;
+  .card {
+    width: 250px;
+    margin: 10px;
+    height: 300px;
+    box-shadow: 0px 1px 5px #ebf3fa;
+    border-radius: 6px;
+    background: none;
+    @media screen and (max-width: 846px) {
+      width: 220px;
+    }
+  }
+`;
+const Loading = () => {
+  return (
+    <Container>
+      <Grids>
+        <div className="card"></div>
+        <div className="card"></div>
+        <div className="card"></div>
+        <div className="card"></div>
+        <div className="card"></div>
+        <div className="card"></div>
+        <div className="card"></div>
+        <div className="card"></div>
+        <div className="card"></div>
+        <div className="card"></div>
+      </Grids>
+      <div className="lds-grid">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </Container>
+  );
+};
+
+export default Loading;
