2d25dcbb74c6bc4ec4bd3516727ab253f699f2d9
root <root@localhost.localdomain>
2021-05-15 12:34:00 +0100

    Add Button Component

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import styled from "styled-components";
+
+const Btn = styled.button`
+  outline: none;
+  border: 2px solid white;
+  color: white;
+  border-radius: 4px;
+  transition: all 0.3s ease-in-out;
+  cursor: pointer;
+  &:hover {
+    background: white !important;
+    border-color: black;
+    color: black;
+  }
+`;
+
+const Button = ({ label, background, padding, fontSize }) => {
+  return (
+    <Btn
+      style={{
+        background: `${background}`,
+        padding: `${padding}`,
+        fontSize: `${fontSize}`,
+      }}
+    >
+      {label}
+    </Btn>
+  );
+};
+
+export default Button;
