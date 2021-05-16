0dab6bf8ab18ba8cb09b6e3b834ec74320e063d5
root <root@localhost.localdomain>
2021-05-16 22:27:30 +0100

    feature: Add trending

--- src/pages/Trending/Trending.jsx
+++ src/pages/Trending/Trending.jsx
@@ -1,41 +1,57 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import PageTitle from "../../shared/PageTitle";
 import { TrendingContainer, TrendingContent } from "./TrendingStyle";
 import Loading from "../../components/Loader/Loading";
+import Card from "../../components/Card/Card";
+import CustomPagination from "../../components/Pagination/CustomPagination";
+import ScrollUpButton from "../../components/Button/ScrollUpButton";
 
 const Trending = () => {
   const [page, setPage] = useState(1);
   const [isLoading, setLoading] = useState(false);
-  const [trending, setTrending] = useState([20, 30, 4564, 7884, 9948]);
-
-  const getTrending = async () => {
+  const [trendings, setTrendings] = useState([]);
+  const [errors, setErrors] = useState("");
+  const getTrendings = async () => {
     setLoading(true);
     try {
       const { data } = await axios.get(
         `https://api.themoviedb.org/3/trending/all/day?api_key=${process.env.REACT_APP_API_KEY}&page=${page}`
       );
       setLoading(false);
-      setTrending(data.results);
-    } catch (err) {}
+      setTrendings(data.results);
+      console.log(data.results);
+    } catch (err) {
+      setErrors(`${err}, the server is unreachable!`);
+    }
   };
 
   useEffect(() => {
-    getTrending();
+    getTrendings();
     // eslint-disable-next-line
   }, [page]);
   return (
     <TrendingContainer>
-      <PageTitle
-        text="TODAY TRENDING"
-        onClick={() => {
-          window.scroll(0, 0);
-        }}
-      />
-      <div style={{ height: "50px" }}></div>
+      <ScrollUpButton onClick={() => window.scroll(0, 0)} />
+      {errors.length > 0 && <div>{errors}</div>}
+      <div style={{ height: "20px" }}></div>
       <TrendingContent>
-        <Loading />
+        {isLoading ? (
+          <Loading />
+        ) : (
+          trendings.map((trending, index) => (
+            <Card
+              key={index}
+              id={trending.id}
+              poster={trending.poster_path}
+              title={trending.title || trending.name}
+              date={trending.first_air_date || trending.release_date}
+              media_type={trending.media_type}
+              vote_average={trending.vote_average}
+            />
+          ))
+        )}
       </TrendingContent>
+      {trendings.length > 0 && <CustomPagination setPage={setPage} />}
     </TrendingContainer>
   );
 };
--- src/pages/Trending/TrendingStyle.jsx
+++ src/pages/Trending/TrendingStyle.jsx
@@ -5,11 +5,14 @@ export const TrendingContainer = styled.div`
   width: 100%;
   padding-top: 120px;
   padding-bottom: 50px;
-  background: white;
+  background: #f0eeee;
 `;
 
 export const TrendingContent = styled.div`
   margin: 0 50px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
   @media screen and (max-width: 900px) {
     margin: 0 10px;
   }
